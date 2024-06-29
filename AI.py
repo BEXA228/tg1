0a967c38d2d01eb8769c63bddba834a31933bd4d
user <yuasha@bk.ru>
2024-06-29 19:07:12 +1000

    123

--- AI.py
+++ AI.py
@@ -81,3 +81,4 @@ def Detection():
 	return Result
 
 # Detection()
+
--- AITrainYolo.py
+++ AITrainYolo.py
@@ -1,22 +1,22 @@
-import os.path
-import time
-from ultralytics import YOLO
-from PIL import Image
+def Detection():
+    from ultralytics import YOLO
+    from PIL import Image
 
-# Обученная модель
-model = YOLO('best.pt')
+    # Обученная модель
+    model = YOLO('best.pt')
 
-# Изображение для проверки
-img = Image.open('photo.jpg')
+    # Изображение для проверки
+    img = Image.open('photo.jpg')
 
-# Предсказание
-results = model(img)
+    # Предсказание
+    results = model(img)
 
-# Сохраняем результат
-results[0].plot()
-output_path = 'output.jpg'
-results[0].save(output_path)
-# while not os.path.exists('output.jpg'):
-#     time.sleep(1)
+    # Сохраняем результат
+    results[0].plot()
+    output_path = 'output.jpg'
+    results[0].save(output_path)
+
+Detection()
+
 
 
