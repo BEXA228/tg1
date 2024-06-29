12b8ddd4deeb69adadbd292d204e33d2573f48e9
user <yuasha@bk.ru>
2024-06-29 21:30:18 +1000

    Itog

--- test1.py
+++ test1.py
@@ -1,15 +1,4 @@
 
-#import subprocess
-
-#cript_path = "test.ps1"  # POWERSHELL SCRIPT PATH
-#commandline_options = ["Powershell.exe", '-ExecutionPolicy', 'Unrestricted', script_path]  # INITIALIZING COMMAND
-
-#result = subprocess.run(commandline_options, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)  # RUN THE SCRIPT USING SUBPROCESS WITH PARAMS
-
-#print(result.stdout)  # PRINT THE STANDARD OUTPUT FROM POWERSHELL SCRIPT
-
-#Detection()
-
 def Detection():
 
     import subprocess
